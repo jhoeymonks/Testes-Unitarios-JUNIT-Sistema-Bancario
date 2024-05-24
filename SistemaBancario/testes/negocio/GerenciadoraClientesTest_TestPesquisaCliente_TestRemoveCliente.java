607d21135ccf7d50ac4e03cd4c1cf5b666d8da27
jhoeymonks <jhoeymonksdev@gmail.com>
2024-05-24 18:36:44 -0300

    Versão 001 - Testes JUNIT - Sistema Bancario

---
+++ SistemaBancario/testes/negocio/GerenciadoraClientesTest_TestPesquisaCliente_TestRemoveCliente.java
@@ -0,0 +1,87 @@
+package negocio;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Classe de teste criada para garantir o funcionamento das principais opera��es
+ * sobre clientes, realizadas pela classe {@link GerenciadoraClientes}.
+ * 
+ * @author Jhoey David
+ * @date 24/05/2024 
+ */
+public class GerenciadoraClientesTest_TestPesquisaCliente_TestRemoveCliente {
+
+	private GerenciadoraClientes gerClientes;
+
+	/**
+	 * Teste basico da pesquisa de um cliente a partir do seu ID.
+	 * 
+	 * @author Jhoey David
+	 * @date 24/05/2024
+	 */
+	@Test
+	public void testPesquisaCliente() {
+
+		/* ========== Montagem do cenario ========== */
+		
+		// criando alguns clientes
+		Cliente cliente01 = new Cliente(1, "Jhoey David", 20, "jhoeymonks@gmail.com", 1, true);
+		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
+		
+		// inserindo os clientes criados na lista de clientes do banco
+		List<Cliente> clientesDoBanco = new ArrayList<>();
+		clientesDoBanco.add(cliente01);
+		clientesDoBanco.add(cliente02);
+		
+		gerClientes = new GerenciadoraClientes(clientesDoBanco);
+
+		/* ========== Execucão ========== */
+		Cliente cliente = gerClientes.pesquisaCliente(1);
+		
+		/* ========== Verificações ========== */
+		assertThat(cliente.getId(), is(1));
+		assertThat(cliente.getEmail(), is("jhoeymonks@gmail.com"));
+		assertThat(cliente.getNome(), is("Jhoey David"));
+		assertThat(cliente.getIdade(), is (34));
+	}
+	
+	/**
+	 * Teste básico da remoção de um cliente a partir do seu ID.
+	 * 
+	 * @author Jhoey David
+	 * @date 24/05/2024
+	 */
+	@Test
+	public void testRemoveCliente() {
+
+		/* ========== Montagem do cenário ========== */
+		
+		// criando alguns clientes
+		Cliente cliente01 = new Cliente(1, "Jhoey David", 20, "jhoeymonks@gmail.com", 1, true);
+		Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmail.com", 1, true);
+		
+		// inserindo os clientes criados na lista de clientes do banco
+		List<Cliente> clientesDoBanco = new ArrayList<>();
+		clientesDoBanco.add(cliente01);
+		clientesDoBanco.add(cliente02);
+		
+		gerClientes = new GerenciadoraClientes(clientesDoBanco);
+		
+		/* ========== Execução ========== */
+		boolean clienteRemovido = gerClientes.removeCliente(2);
+		
+		/* ========== Verificações ========== */
+		assertThat(clienteRemovido, is(true));
+		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
+		assertNull(gerClientes.pesquisaCliente(2));
+		
+	}
+	
+}
+
